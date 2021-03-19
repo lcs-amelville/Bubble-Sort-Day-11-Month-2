b91ad87cdf95196d17121b86e219257351c78588
Aidan Melville <amelville@lcs.on.ca>
2021-03-19 14:32:44 -0400

    Finished original BubbleSort

--- Bubble Sort Day 11 Month 2/main.swift
+++ Bubble Sort Day 11 Month 2/main.swift
@@ -7,5 +7,55 @@
 
 import Foundation
 
-print("Hello, World!")
+// Pause to allow user to see output
+func waitForInput() {
+    print("Press RETURN to contiue...")
+    _ = readLine()
+}
 
+// Create an empty array
+var dataSet: [Int] = []
+
+// Populate Array
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
+
+//Print the unsorted array
+print("Unsorted:")
+print(dataSet)
+waitForInput()
+
+
+// Sort the array
+
+// Loop through the array "n" times
+//(However many times there are elements in the array)
+for i in 0..<dataSet.count {
+    
+    // One pass through the array to float the highest number to the end
+    for j in 0..<dataSet.count - 1 {
+        
+        // Compare left value to right value
+        if dataSet[j] > dataSet[j + 1] {
+            
+            //Swap value when the left value is greater than the right value
+            let temperaryValue = dataSet[j]     //Set aside the left value
+            dataSet[j] = dataSet[j + 1]         // Replace the left with right
+            dataSet[j + 1] = temperaryValue     // Replace right with temporary value
+        }
+        
+    }
+
+    // Print the aray after "n" passes
+    print("Array after pass \(i + 1)")
+    print(dataSet)
+    waitForInput()
+
+}
+
+
+// Print Sorted Array
+print("Sorted:")
+print(dataSet)
+waitForInput()
