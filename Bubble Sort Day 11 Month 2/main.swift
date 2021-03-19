b626038ebfed7341e66125334b20719384a527eb
Aidan Melville <amelville@lcs.on.ca>
2021-03-19 15:05:26 -0400

    Both improvements are working.

--- Bubble Sort Day 11 Month 2/main.swift
+++ Bubble Sort Day 11 Month 2/main.swift
@@ -27,26 +27,40 @@ print(dataSet)
 waitForInput()
 
 
+// New variable for the array which reduces the passes each pass.
+var unsorted = dataSet.count - 1
+var swap = false
 // Sort the array
 
 // Loop through the array "n" times
 //(However many times there are elements in the array)
 for i in 0..<dataSet.count {
     
+    
     // One pass through the array to float the highest number to the end
-    for j in 0..<dataSet.count - 1 {
+    for j in 0..<unsorted {
+      swap = false
+        //What comparison is this?
+        print("Pass \(i + 1), comparison \(j + 1)...")
         
         // Compare left value to right value
         if dataSet[j] > dataSet[j + 1] {
-            
+           
+            swap = true
             //Swap value when the left value is greater than the right value
             let temperaryValue = dataSet[j]     //Set aside the left value
             dataSet[j] = dataSet[j + 1]         // Replace the left with right
             dataSet[j + 1] = temperaryValue     // Replace right with temporary value
         }
-        
+       
     }
-
+    if swap == false {
+        break
+    }
+    
+    // Lower the variable by 1
+    unsorted -= 1
+    
     // Print the aray after "n" passes
     print("Array after pass \(i + 1)")
     print(dataSet)
@@ -59,3 +73,4 @@ for i in 0..<dataSet.count {
 print("Sorted:")
 print(dataSet)
 waitForInput()
+
