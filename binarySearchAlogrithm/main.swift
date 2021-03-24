987898b4e6799ca6954c3ca96c21bdced6f1b45e
Aidan Melville <amelville@lcs.on.ca>
2021-03-24 10:34:54 -0400

    Added in print statements that printed all the values and an index.

--- binarySearchAlogrithm/main.swift
+++ binarySearchAlogrithm/main.swift
@@ -28,33 +28,49 @@ var sizeOfArray = dataSet.count
 var startOfArray = 0
 var positionOfLastElement = dataSet.count - 1
 
-    
+
 for _ in 0...dataSet.count {
-        
+    
     let middleOfArray = (positionOfLastElement + startOfArray) / 2
     let middleNumber = dataSet[middleOfArray]
-        
-        if  searchNumber == middleNumber {
-            
-            print("the target number is \(middleOfArray) position")
-            break
-            
-        } else if searchNumber > middleNumber {
-          
-          startOfArray = middleOfArray + 1
-          
-        } else  if searchNumber < middleNumber {
-            
-            positionOfLastElement = middleOfArray - 1
-            
-        } else if startOfArray > positionOfLastElement {
-            print("The number isn't in this array")
-            break
-        }
-        
-      
-        
+    print("-----")
+    print("The target number is \(searchNumber)")
+    print("The start of the array is \(startOfArray)")
+    print("The middle of the array is \(middleOfArray)")
+    print("The end of the array is \(positionOfLastElement)")
+    print("-----")
+    print("------")
+    print("Index:\t", terminator: "")
+    for i in startOfArray...positionOfLastElement {
+        print("\(i)\t", terminator: "")
     }
+    print("")
+    print("Values: ", terminator: "")
+    for i in startOfArray...positionOfLastElement {
+        print("\(dataSet[i])\t", terminator: "")
+    }
+    print("")
+    if  searchNumber == middleNumber {
+        
+        print("the target number is in position \(middleOfArray)")
+        break
+        
+    } else if searchNumber > middleNumber {
+        
+        startOfArray = middleOfArray + 1
+        
+    } else  if searchNumber < middleNumber {
+        
+        positionOfLastElement = middleOfArray - 1
+        
+    } else if startOfArray > positionOfLastElement {
+        print("The number isn't in this array")
+        break
+    }
+    
+    
+    
+}
 
 
 
